b09a9e25c599c2f968979664d9c483ba3d3b7ce9
shrutiomar <shrutigupta2596@gmail.com>
2025-05-14 20:54:02 +0530

    changes

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import TotalScore from "./TotalScore";
-import NumberSelector from "./NumberSelector";
 import styled from "styled-components";
+import NumberSelector from "./NumberSelector";
+import TotalScore from "./TotalScore";
 import RoleDice from "./RoleDice";
 import { useState } from "react";
-import { Button } from "../styled/Button";
+import { Button, OutlineButton } from "../styled/Button";
+import Rules from "./Rules";
 
 const GamePlay = () => {
   const [score, setScore] = useState(0);
   const [selectedNumber, setSelectedNumber] = useState();
   const [currentDice, setCurrentDice] = useState(1);
   const [error, setError] = useState("");
+  const [showRules, setShowRules] = useState(false);
 
   const generateRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
@@ -18,21 +19,24 @@ const GamePlay = () => {
 
   const roleDice = () => {
     if (!selectedNumber) {
-      setError("You have not Selected any Number");
-
+      setError("You have not selected any number");
       return;
     }
-    setError("");
-    const randomNumber = generateRandomNumber(1, 7);
 
+    const randomNumber = generateRandomNumber(1, 7);
     setCurrentDice((prev) => randomNumber);
 
-    if (selectedNumber == randomNumber) {
+    if (selectedNumber === randomNumber) {
       setScore((prev) => prev + randomNumber);
     } else {
       setScore((prev) => prev - 2);
     }
+
     setSelectedNumber(undefined);
+  };
+
+  const resetScore = () => {
+    setScore(0);
   };
 
   return (
@@ -40,17 +44,21 @@ const GamePlay = () => {
       <div className="top_section">
         <TotalScore score={score} />
         <NumberSelector
-          setError={setError}
           error={error}
+          setError={setError}
           selectedNumber={selectedNumber}
           setSelectedNumber={setSelectedNumber}
         />
       </div>
       <RoleDice currentDice={currentDice} roleDice={roleDice} />
       <div className="btns">
-        <Button>Reset</Button>
-        <Button>Show Rules</Button>
+        <OutlineButton onClick={resetScore}>Reset Score</OutlineButton>
+        <Button onClick={() => setShowRules((prev) => !prev)}>
+          {showRules ? "Hide" : "Show"} Rules
+        </Button>
       </div>
+
+      {showRules && <Rules />}
     </MainContainer>
   );
 };
@@ -59,13 +67,25 @@ export default GamePlay;
 
 const MainContainer = styled.main`
   padding-top: 70px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: hidden;
   .top_section {
     display: flex;
     justify-content: space-around;
     align-items: end;
+    width:100%;
   }
-  .btns{
+  .btns {
+    margin-top: 20px;
+    gap: 10px;
     display: flex;
     flex-direction: column;
+    align-items: center;
+    /* justify-content: center; */
+    gap: 10px;
+    margin-right: 320px;
   }
 `;
--- src/styled/Button.js
+++ src/styled/Button.js
@@ -20,4 +20,13 @@ export const Button = styled.button`
   }
   `;
   export const OutlineButton = styled(Button)`
+   background-color: white;
+  border: 1px solid black;
+  color: black;
+   &:hover {
+    background-color: black;
+    border: 1px solid transparent;
+    color: white;
+   
+  }
   `;
