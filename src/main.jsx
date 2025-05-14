932bc59d455795be83244ac2239a9d78274c3abf
Shruti Gupta <shrutigupta2596@gmail.com>
2025-05-14 21:10:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import logo from "./assets/logo.png";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
